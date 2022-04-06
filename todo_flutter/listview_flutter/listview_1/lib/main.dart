08c0935bc9b0ae90ea8f25ae4dfd93a9c242ef0e
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2022-04-07 02:15:38 +0900

    02:13 04/07/2022
    코틀린 버전 upgrade
    더미데이터 리스트 3개 생성 및 활용
    column 위젯 활용 리스트 생성
    //Text 좌측 정렬 실패

--- todo_flutter/listview_flutter/listview_1/lib/main.dart
+++ todo_flutter/listview_flutter/listview_1/lib/main.dart
@@ -19,6 +19,25 @@ class MyhomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<int> colorCodes = <int>[700, 600, 500, 400, 300, 200, 100];
+    final List<String> name = <String>[
+      'Day1',
+      'Day2',
+      'Day3',
+      'Day4',
+      'Day5',
+      'Day6',
+      'Day7'
+    ];
+    final List<String> todo = <String>[
+      'ToDo1',
+      'ToDo2',
+      'ToDo3',
+      'ToDo4',
+      'ToDo5',
+      'ToDo6',
+      'ToDo7'
+    ];
     return Scaffold(
         appBar: AppBar(
           title: Text('List-View'),
@@ -40,34 +59,65 @@ class MyhomePage extends StatelessWidget {
                 ],
               )
             ]),
-            ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(8),
+            Column(
               children: [
-                Container(
-                  height: 50,
-                  color: Colors.amber[600],
-                  child: const Center(child: Text('data A')),
-                ),
-                Container(
-                  height: 50,
-                  color: Colors.blue[600],
-                  child: const Center(child: Text('data B')),
-                ),
-                Container(
-                  height: 50,
-                  color: Colors.green[600],
-                  child: const Center(child: Text('data C')),
-                ),
-                Container(
-                  height: 50,
-                  color: Colors.red[600],
-                  child: const Center(child: Text('data D')),
-                ),
-                Container(
-                  height: 50,
-                  color: Colors.white,
-                  child: const Center(child: Text('data E')),
+                ListView.separated(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: 7,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        color: Colors.amber[colorCodes[index]],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.date_range,
+                                ),
+                                Text(
+                                  name[index],
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              todo[index],
+                              style: TextStyle(fontSize: 18),
+                            )
+                          ],
+                        ));
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                  // children: [
+                  //   Container(
+                  //     height: 50,
+                  //     color: Colors.amber[600],
+                  //     child: const Center(child: Text('data A')),
+                  //   ),
+                  //   Container(
+                  //     height: 50,
+                  //     color: Colors.blue[600],
+                  //     child: const Center(child: Text('data B')),
+                  //   ),
+                  //   Container(
+                  //     height: 50,
+                  //     color: Colors.green[600],
+                  //     child: const Center(child: Text('data C')),
+                  //   ),
+                  //   Container(
+                  //     height: 50,
+                  //     color: Colors.red[600],
+                  //     child: const Center(child: Text('data D')),
+                  //   ),
+                  //   Container(
+                  //     height: 50,
+                  //     color: Colors.white,
+                  //     child: const Center(child: Text('data E')),
+                  //   ),
+                  // ],
                 ),
               ],
             ),
